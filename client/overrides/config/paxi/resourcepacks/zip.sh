1489fa15141ba62f750940ea7a1a8d4f5c835e6d
Bill Parrott <bill@chimericdream.com>
2024-12-16 09:22:13 -0600

    feat: bring back old patchouli guide as a starting point

--- client/overrides/config/paxi/resourcepacks/zip.sh
+++ client/overrides/config/paxi/resourcepacks/zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#cd world-of-pannotia;7z a -tzip ../world-of-pannotia.zip .;cd ..
+cd world-of-pannotia;7z a -tzip ../world-of-pannotia.zip .;cd ..
 cd wyvernity;7z a -tzip ../wyvernity.zip .;cd ..
 
-#rm -rf world-of-pannotia
+rm -rf world-of-pannotia
 rm -rf wyvernity
